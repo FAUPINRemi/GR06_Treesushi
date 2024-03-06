6d586403483264aa04dd8adc97ce2761c9f3e84c
FAUPINRemi <115220441+FAUPINRemi@users.noreply.github.com>
2024-03-06 13:45:59 +0100

    Maj front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AproposComponent } from './pages/apropos/apropos.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { AccueilComponent } from './pages/accueil/accueil.component'; // Assurez-vous d'importer le composant Accueil
+
 const routes: Routes = [
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+  { path: 'accueil', component: AccueilComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'a-propos', component: AproposComponent },
-  
-  
 ];
 
 @NgModule({
