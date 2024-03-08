2c6d94428549730bc4aa04df7ec4c8a329116380
FAUPINRemi <115220441+FAUPINRemi@users.noreply.github.com>
2024-03-09 00:37:56 +0100

    Page choix ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AproposComponent } from './pages/apropos/apropos.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { AccueilComponent } from './pages/accueil/accueil.component'; 
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { MenuaespComponent } from './pages/menuaesp/menuaesp.component';
 const routes: Routes = [
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
   { path: 'accueil', component: AccueilComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'a-propos', component: AproposComponent },
+  { path: 'menu-choix', component: MenuaespComponent },
   { path: '404', component: ErrorPageComponent },
   { path: '**', redirectTo: '404' }
 ];
