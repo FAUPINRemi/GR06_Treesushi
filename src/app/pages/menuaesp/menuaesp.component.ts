147841d9afb213b9bf1d71d3328fe0143501915d
FAUPINRemi <115220441+FAUPINRemi@users.noreply.github.com>
2024-03-08 23:28:04 +0100

    Error-pas ok

---
+++ src/app/pages/menuaesp/menuaesp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menuaesp',
+  templateUrl: './menuaesp.component.html',
+  styleUrl: './menuaesp.component.css'
+})
+export class MenuaespComponent {
+
+}
