963a1b8de82782944d667617545f98473732d790
FAUPINRemi <115220441+FAUPINRemi@users.noreply.github.com>
2024-03-09 17:06:35 +0100

    Hebergement db api

--- src/API/database/database.php
+++ src/API/database/database.php
@@ -1,10 +1,10 @@
 <?php
 class Database
 {
-    private $serveur = "https://treesushi-api.000webhostapp.com";
-    private $utilisateur = "id21969745_treesushiapi";
-    private $mot_de_passe  = "TreesushiAPI1234?";
-    private $base_de_donnees = "id21969745_testapi3";
+    private $serveur = "185.27.134.143";
+    private $utilisateur = "if0_36130759";
+    private $mot_de_passe  = "8d0uUnRMLj";
+    private $base_de_donnees = "if0_36130759_apitreesushi";
     public $connexion;
 
 
