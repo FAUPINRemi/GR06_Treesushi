b83518f0f87624b1f9e7e3933be1dcd2268ee213
Skiioo <merletti.sacha@gmail.com>
2024-03-21 12:26:07 +0100

    ajout BDD 21/03/24

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,15 +3,16 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
-})
+}) 
+
 export class DataService {
   constructor(private http: HttpClient) { }
 
   getData() {
-    return this.http.get('https://treesushi-api.000webhostapp.com/API/Controler/Menu/read.php');
+    return this.http.get('http://localhost/htdocs/SAE401/GR06_Treesushi/src/API/Controler/Menu/read.php');
   }
 
   createMenu(data: any) {
-    return this.http.post('https://treesushi-api.000webhostapp.com/API/models/create.php', data);
+    return this.http.post('https://treesushi-api.000webhostapp.com/API/Controler/Menu/create.php', data);
   }
 }
