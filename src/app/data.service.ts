14a9f3a55ab0c12e7549585430231cc221d0d533
FAUPINRemi <115220441+FAUPINRemi@users.noreply.github.com>
2024-03-23 20:48:13 +0100

    Fixe bug 23/03 20h48

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders,  } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,13 +10,19 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getData() {
-    return this.http.get('http://localhost/SAE401/GR06_Treesushi/src/API/Controler/Menu/read.php');
+    return this.http.get('http://localhost/GR06_Treesushi/src/API/Controler/Menu/read.php');
   }
 
   
   getOneData(id: any){
-    return this.http.get(`http://localhost/SAE401/GR06_Treesushi/src/API/Controler/Menu/read_One.php${id}`)
+    return this.http.get(`http://localhost/GR06_Treesushi/src/API/Controler/Menu/read_One.php?id=${id}`)
   }
+  readAliment(id: any){
+    return this.http.get(`http://localhost/GR06_Treesushi/src/API/Controler/Aliment/read.php?id=${id}`)
+}
+readSaveur(id: any){
+    return this.http.get(`http://localhost/GR06_Treesushi/src/API/Controler/Saveur/read.php?id=${id}`)
+}
 
 
   createMenu(data: any) {
@@ -30,6 +36,7 @@ export class DataService {
     return this.http.post('http://localhost/GR06_Treesushi/src/API/Controler/Menu/create.php', data, { headers });
   }
 
+
   deleteMenu(id: number): Observable<any> {
     const options = {
       headers: new HttpHeaders({
@@ -39,9 +46,7 @@ export class DataService {
         id: id
       },
     };
-  
+
     return this.http.delete('http://localhost/htdocs/SAE401/GR06_Treesushi/src/API/Controler/Menu/delete.php', options);
   }
-
 }
-
